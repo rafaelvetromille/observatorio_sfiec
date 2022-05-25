714e100861f4a407652cf4cf3d7b1f755f091442
Rafael Vetromillez <rafaelvetromille1@gmail.com>
2022-05-25 13:15:57 -0300

    add

--- problema1.R
+++ problema1.R
@@ -9,6 +9,26 @@ library(tidyverse)
 library(geobr)
 library(ggspatial)
 
+# Temas de todos os mapas
+tema_mapa <- theme_bw() + 
+  theme(
+    axis.text.y = element_text(
+      angle = 90,
+      hjust = 0.5,
+      size = 8
+    ),
+    axis.text.x = element_text(size = 8),
+    axis.title.y = element_text(size = rel(0.8)),
+    axis.title.x = element_text(size = rel(0.8)),
+    panel.grid.major = element_line(
+      color = gray(0.9),
+      linetype = "dashed",
+      size = 0.1
+    ),
+    panel.background = element_rect(fill = "white") +
+      annotation_scale(location = "br", width_hint = 0.30)
+  )
+
 # Defina o seu projeto no Google Cloud
 set_billing_id('observatoriosfiec')
 
@@ -29,42 +49,23 @@ rais <- bd_collect(query_rais)
 # Pergunta 1: 
 # Em quais municípios a indústria de calçados mais emprega no estado do Ceará?
 
-df1 <- dplyr::full_join(
-  
-  geobr::read_municipality(year = '2020'),
-  
-  rais %>%
-    dplyr::filter(stringr::str_detect(cnae_2, '153')) %>%
-    dplyr::group_by(id_municipio) %>%
-    dplyr::summarise(n = dplyr::n()) %>%
-    dplyr::mutate(code_muni = as.numeric(id_municipio), .keep = 'unused', .before = dplyr::everything()) %>%
-    dplyr::arrange(dplyr::desc(n)),
-  
-  by = 'code_muni'
-  
-) %>% 
+df1 <- geobr::read_municipality(year = '2020') %>%
+  dplyr::full_join(
+    rais %>%
+      dplyr::filter(stringr::str_detect(cnae_2, '153')) %>%
+      dplyr::group_by(id_municipio) %>%
+      dplyr::summarise(n = dplyr::n()) %>%
+      dplyr::mutate(
+        code_muni = as.numeric(id_municipio),
+        .keep = 'unused',
+        .before = dplyr::everything()
+      ) %>%
+      dplyr::arrange(dplyr::desc(n)),
+    by = 'code_muni'
+  ) %>%
   dplyr::filter(abbrev_state == 'CE')
 
 # Mapa 1 - Os munícipios que mais empregram no setor de calçados
-
-tema_mapa <- theme_bw() + 
-  theme(
-    axis.text.y = element_text(
-      angle = 90,
-      hjust = 0.5,
-      size = 8
-    ),
-    axis.text.x = element_text(size = 8),
-    axis.title.y = element_text(size = rel(0.8)),
-    axis.title.x = element_text(size = rel(0.8)),
-    panel.grid.major = element_line(
-      color = gray(0.9),
-      linetype = "dashed",
-      size = 0.1
-    ),
-    panel.background = element_rect(fill = "white") +
-      annotation_scale(location = "br", width_hint = 0.30)
-  )
 
 df1 %>%
   ggplot() +
@@ -102,24 +103,24 @@ df1 %>%
 # Pergunta 2: 
 # Qual a média salarial e número de empregados da indústria de calçados nestes municípios?
 
-df2 <- dplyr::full_join(
-  
-  geobr::read_municipality(),
-  
-  rais %>%
-    dplyr::filter(stringr::str_detect(cnae_2, '153')) %>%
-    dplyr::group_by(id_municipio) %>%
-    dplyr::summarise(
-      n = dplyr::n(),
-      remuneracao_media = mean(valor_remuneracao_media, na.rm = TRUE)
-    ) %>%
-    dplyr::arrange(desc(n)) %>%
-    dplyr::mutate(code_muni = as.numeric(id_municipio), .keep = 'unused', .before = dplyr::everything()) %>% 
-    dplyr::arrange(desc(n)),
-    
-  by = 'code_muni'
-  
-) %>% 
+df2 <- geobr::read_municipality(year = '2020') %>%
+  dplyr::full_join(
+    rais %>%
+      dplyr::filter(stringr::str_detect(cnae_2, '153')) %>%
+      dplyr::group_by(id_municipio) %>%
+      dplyr::summarise(
+        n = dplyr::n(),
+        remuneracao_media = mean(valor_remuneracao_media, na.rm = TRUE)
+      ) %>%
+      dplyr::arrange(desc(n)) %>%
+      dplyr::mutate(
+        code_muni = as.numeric(id_municipio),
+        .keep = 'unused',
+        .before = dplyr::everything()
+      ) %>%
+      dplyr::arrange(desc(n)),
+    by = 'code_muni'
+  ) %>%
   dplyr::filter(abbrev_state == 'CE')
 
 # Mapa 2 - Média salarial dos municípios
@@ -162,31 +163,27 @@ df2 %>%
 # Pergunta 3:
 # Quais as principais ocupações empregadas no setor (indústria calçadista) e qual a média salarial delas?
 
-df3 <- dplyr::left_join(
-  
-  rais %>%
-    dplyr::filter(stringr::str_detect(cnae_2, '153')) %>%
-    dplyr::group_by(cbo_2002) %>%
-    dplyr::summarise(
-      n = dplyr::n(),
-      remuneracao_media = mean(valor_remuneracao_media, na.rm = TRUE)
-    ) %>%
-    dplyr::arrange(desc(n)),
-  
-  readr::read_delim(
-    file = 'CBO2002 - Ocupacao.csv',
-    delim = ';',
-    locale = locale(encoding = 'Latin1'),
-    show_col_types = FALSE
+df3 <- rais %>%
+  dplyr::filter(stringr::str_detect(cnae_2, '153')) %>%
+  dplyr::group_by(cbo_2002) %>%
+  dplyr::summarise(
+    n = dplyr::n(),
+    remuneracao_media = mean(valor_remuneracao_media, na.rm = TRUE)
   ) %>%
-    janitor::clean_names() %>%
-    dplyr::rename(cbo_2002 = codigo, ocupacao = titulo),
-  
-  by = 'cbo_2002'
-  
-) 
+  dplyr::arrange(desc(n)) %>%
+  dplyr::left_join(
+    readr::read_delim(
+      file = 'CBO2002 - Ocupacao.csv',
+      delim = ';',
+      locale = locale(encoding = 'Latin1'),
+      show_col_types = FALSE
+    ) %>%
+      janitor::clean_names() %>%
+      dplyr::rename(cbo_2002 = codigo, ocupacao = titulo),
+    by = 'cbo_2002'
+  ) 
 
-# Tabela 3 - Remuneração média por ocupação
+# Tabela 3 - Quantidade e remuneração média por ocupação
 
 df3 %>% 
   dplyr::select(4, 2, 3) %>% 
@@ -219,6 +216,20 @@ df4 <- dplyr::left_join(
   
 )
 
+# Tabela 4 - Número de funcionário no setor, por sexo 
+
+df4 %>% 
+  dplyr::select(sexo_nome, n, remuneracao_media) %>% 
+  dplyr::rename(
+    `Sexo` = 1, 
+    `Qtd.` = 2, 
+    `Remuneração Média (R$)` = 3
+  ) %>% 
+  dplyr::bind_rows(summarise(.,
+                             across(.cols = 2, ~sum(.x, na.rm = TRUE)),
+                             across(where(is.character), ~"Total"), 
+                             across(.cols = 3, ~mean(.x, na.rm = TRUE))))
+
 # Pergunta 5: 
 # Qual o nível de escolaridade dos empregados neste setor (indústria calçadista)?
 
@@ -228,7 +239,8 @@ df5 <- dplyr::left_join(
     dplyr::filter(stringr::str_detect(cnae_2, '153')) %>% 
     dplyr::group_by(grau_instrucao_apos_2005) %>%
     dplyr::summarise(
-      n = dplyr::n()
+      n = dplyr::n(),
+      remuneracao_media = mean(valor_remuneracao_media, na.rm = T)
     ) %>% 
     dplyr::arrange(dplyr::desc(n)) %>% 
     dplyr::rename(chave = grau_instrucao_apos_2005),
@@ -241,3 +253,29 @@ df5 <- dplyr::left_join(
   by = 'chave'
   
 )
+
+# Tabela 5 - Grau de instrução, quantidade e média salarial
+
+df5 %>% 
+  dplyr::mutate(
+    grau_instrucao = case_when(
+      grau_instrucao == 'MEDIO COMPL' ~ 'Ensino Médio Completo', 
+      grau_instrucao == 'MEDIO INCOMP' ~ 'Ensimo Médio Incompleto', 
+      grau_instrucao == 'FUND COMPL' ~ 'Fundamental Completo', 
+      grau_instrucao == '6. A 9. FUND' ~ 'De 6º ao 9º do Ensino Fundamental', 
+      grau_instrucao == 'ATE 5.A INC' ~ 'Até 5º ano Incompleto', 
+      grau_instrucao == 'SUP. INCOMP' ~ 'Ensino Superior Incompleto', 
+      grau_instrucao == 'SUP. COMP' ~ 'Ensio Superior Completo', 
+      grau_instrucao == '5.A CO FUND' ~ 'Até o 5º Completo', 
+      grau_instrucao == 'ANALFABETO' ~ 'Analfabeto', 
+      grau_instrucao == 'MESTRADO' ~ 'Mestrado', 
+      grau_instrucao == 'DOUTORADO' ~ 'Doutorado'
+    )
+  ) %>%
+  dplyr::mutate(remuneracao_media = scales::dollar(remuneracao_media, prefix = 'R$ ', decimal.mark = ',')) %>% 
+  dplyr::select(4, 2, 3) %>% 
+  dplyr::rename(
+    `Grau de Instrução` = 1, 
+    `Qtd.` = 2, 
+    `Remuneração Média (R$)` = 3
+  )

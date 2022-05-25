a23fb302fb59f27f676750e4534f16e9d42bc5cc
Rafael Vetromillez <rafaelvetromille1@gmail.com>
2022-05-24 22:47:36 -0300

    Initial commit

---
+++ comex.R
@@ -0,0 +1,208 @@
+library(tidyverse)
+
+## ncm
+ncm <- readr::read_delim(file = 'https://balanca.economia.gov.br/balanca/bd/tabelas/NCM.csv', 
+                         delim = ';', locale = locale(encoding = 'Latin1'), 
+                         show_col_types = FALSE) %>%
+  dplyr::select(CO_NCM, NO_NCM_POR)
+
+## Quais os top 10 produtos exportados pelo Ceará em 2019 e o desempenho do
+## comércio desses produtos em 2021, houve uma queda ou crescimento das exportações
+## desses produtos?
+
+## 2019
+exp_2019 <- readr::read_csv2(
+  file = 'https://balanca.economia.gov.br/balanca/bd/comexstat-bd/ncm/EXP_2019.csv'
+  )
+
+dplyr::left_join(
+  
+  exp_2019 %>% 
+    dplyr::filter(SG_UF_NCM == "CE") %>% 
+    dplyr::mutate(DATE = lubridate::make_date(year = CO_ANO, month = CO_MES)) %>% 
+    dplyr::group_by(CO_NCM) %>% 
+    dplyr::summarise(
+      across(.cols = c(KG_LIQUIDO, VL_FOB), .fns = sum)
+    ) %>% 
+    dplyr::arrange(desc(VL_FOB)) %>% 
+    dplyr::mutate(PART = scales::percent(VL_FOB / sum(VL_FOB), 0.01)) %>% 
+    dplyr::rename_with(.cols = c(KG_LIQUIDO, VL_FOB), .fn = ~paste0(.x, '_2019')), 
+  
+  ncm, 
+  
+  by = 'CO_NCM'
+
+)
+
+
+## 2021
+exp_2021 <- readr::read_csv2(
+  file = 'https://balanca.economia.gov.br/balanca/bd/comexstat-bd/ncm/EXP_2021.csv'
+)
+
+dplyr::left_join(
+  
+  exp_2021 %>% 
+    dplyr::filter(SG_UF_NCM == "CE") %>% 
+    dplyr::mutate(DATE = lubridate::make_date(year = CO_ANO, month = CO_MES)) %>% 
+    dplyr::group_by(CO_NCM) %>% 
+    dplyr::summarise(
+      across(.cols = c(KG_LIQUIDO, VL_FOB), .fns = sum)
+    ) %>% 
+    dplyr::arrange(desc(VL_FOB)) %>% 
+    dplyr::mutate(PART = scales::percent(VL_FOB / sum(VL_FOB), 0.01)) %>% 
+    dplyr::rename_with(.cols = c(KG_LIQUIDO, VL_FOB), .fn = ~paste0(.x, '_2021')), 
+  
+  ncm, 
+  
+  by = 'CO_NCM'
+  
+)
+
+### Quais os top 10 produtos importados pelo Ceará em 2019 e o desempenho do
+### comércio desses produtos em 2021, houve uma queda ou crescimento das importações
+### desses produtos?
+
+## 2019
+imp_2019 <- readr::read_csv2(
+  file = 'https://balanca.economia.gov.br/balanca/bd/comexstat-bd/ncm/IMP_2019.csv'
+)
+
+dplyr::left_join(
+  
+  imp_2019 %>% 
+    dplyr::filter(SG_UF_NCM == "CE") %>% 
+    dplyr::mutate(DATE = lubridate::make_date(year = CO_ANO, month = CO_MES)) %>% 
+    dplyr::group_by(CO_NCM) %>% 
+    dplyr::summarise(
+      across(.cols = c(KG_LIQUIDO, VL_FOB), .fns = sum)
+    ) %>% 
+    dplyr::arrange(desc(VL_FOB)) %>% 
+    dplyr::mutate(PART = scales::percent(VL_FOB / sum(VL_FOB), 0.01)) %>% 
+    dplyr::rename_with(.cols = c(KG_LIQUIDO, VL_FOB), .fn = ~paste0(.x, '_2019')), 
+  
+  
+  ncm, 
+  
+  by = 'CO_NCM'
+  
+)
+
+
+## 2021
+imp_2021 <- readr::read_csv2(
+  file = 'https://balanca.economia.gov.br/balanca/bd/comexstat-bd/ncm/IMP_2021.csv'
+)
+
+dplyr::left_join(
+  
+  imp_2021 %>% 
+    dplyr::filter(SG_UF_NCM == "CE") %>% 
+    dplyr::mutate(DATE = lubridate::make_date(year = CO_ANO, month = CO_MES)) %>% 
+    dplyr::group_by(CO_NCM) %>% 
+    dplyr::summarise(
+      across(.cols = c(KG_LIQUIDO, VL_FOB), .fns = sum)
+    ) %>% 
+    dplyr::arrange(desc(VL_FOB)) %>% 
+    dplyr::mutate(PART = scales::percent(VL_FOB / sum(VL_FOB), 0.01)) %>% 
+    dplyr::rename_with(.cols = c(KG_LIQUIDO, VL_FOB), .fn = ~paste0(.x, '_2021')), 
+
+  ncm, 
+  
+  by = 'CO_NCM'
+  
+)
+
+### Países
+paises <- readr::read_delim(file = 'https://balanca.economia.gov.br/balanca/bd/tabelas/PAIS.csv', 
+                            delim = ';', locale = locale(encoding = 'Latin1'), 
+                            show_col_types = FALSE) %>% 
+  dplyr::select(CO_PAIS, NO_PAIS)
+
+
+### Quais os top 5 destinos (países) dos produtos exportados pelo Ceará em 2019? Esses
+### países perderam ou ganharam participação em 2021?
+
+
+### 2019 
+left_join(
+  
+  exp_2019 %>%
+    dplyr::filter(SG_UF_NCM == "CE") %>% 
+    dplyr::group_by(CO_PAIS) %>% 
+    dplyr::summarise(
+      across(.cols = c(KG_LIQUIDO, VL_FOB), .fns = sum)
+    ) %>% 
+    dplyr::arrange(desc(KG_LIQUIDO)) %>% 
+    dplyr::mutate(PART = scales::percent(VL_FOB / sum(VL_FOB), 0.01)) %>%
+    dplyr::rename_with(.cols = c(KG_LIQUIDO, VL_FOB), .fn = ~paste0(.x, '_2019')), 
+  
+  paises,
+  
+  by = 'CO_PAIS'
+
+)
+
+### 2021
+left_join(
+  
+  exp_2021 %>%
+    dplyr::filter(SG_UF_NCM == "CE") %>% 
+    dplyr::group_by(CO_PAIS) %>% 
+    dplyr::summarise(
+      across(.cols = c(KG_LIQUIDO, VL_FOB), .fns = sum)
+    ) %>% 
+    dplyr::arrange(desc(KG_LIQUIDO)) %>%
+    dplyr::mutate(PART = scales::percent(VL_FOB / sum(VL_FOB), 0.01)) %>% 
+    dplyr::rename_with(.cols = c(KG_LIQUIDO, VL_FOB), .fn = ~paste0(.x, '_2021')), 
+
+  paises,
+  
+  by = 'CO_PAIS'
+  
+)
+
+
+### Quais as top 5 origens (países) dos produtos importados pelo Ceará em 2019? Esses
+### países perderam ou ganharam participação em 2021?
+
+### 2019
+
+left_join(
+  
+  imp_2019 %>%
+    dplyr::filter(SG_UF_NCM == "CE") %>% 
+    dplyr::group_by(CO_PAIS) %>% 
+    dplyr::summarise(
+      across(.cols = c(KG_LIQUIDO, VL_FOB), .fns = sum)
+    ) %>% 
+    dplyr::arrange(desc(VL_FOB)) %>% 
+    dplyr::mutate(PART = scales::percent(VL_FOB / sum(VL_FOB), 0.01)) %>% 
+    dplyr::rename_with(.cols = c(KG_LIQUIDO, VL_FOB), .fn = ~paste0(.x, '_2019')),
+  
+  paises,
+  
+  by = 'CO_PAIS'
+  
+)
+
+
+### 2021
+
+left_join(
+  
+  imp_2021 %>%
+    dplyr::filter(SG_UF_NCM == "CE") %>% 
+    dplyr::group_by(CO_PAIS) %>% 
+    dplyr::summarise(
+      across(.cols = c(KG_LIQUIDO, VL_FOB), .fns = sum)
+    ) %>% 
+    dplyr::arrange(desc(VL_FOB)) %>% 
+    dplyr::mutate(PART = scales::percent(VL_FOB / sum(VL_FOB), 0.01)) %>% 
+    dplyr::rename_with(.cols = c(KG_LIQUIDO, VL_FOB), .fn = ~paste0(.x, '_2021')), 
+  
+  paises,
+  
+  by = 'CO_PAIS'
+  
+)
---
+++ rais.R
@@ -0,0 +1,132 @@
+# Carrega os pacotes
+library("basedosdados")
+library("tidyverse")
+library("geobr")
+library("pdftools")
+
+# Defina o seu projeto no Google Cloud
+set_billing_id("observatoriosfiec")
+
+# Para carregar o dado direto no R
+query_rais <- bdplyr("br_me_rais.microdados_vinculos") %>%
+  dplyr::filter(ano == 2020, sigla_uf == "CE") %>% 
+  dplyr::select(ano, sigla_uf, valor_remuneracao_media, 
+                nacionalidade, tipo_vinculo, id_municipio, 
+                cnae_2, cbo_2002, sexo, grau_instrucao_apos_2005) 
+  
+# Realizar o query
+rais <- bd_collect(query_rais) 
+
+## Em quais municípios a indústria de calçados mais emprega no estado do Ceará?
+dplyr::left_join(
+  
+  rais %>% 
+    dplyr::filter(stringr::str_detect(cnae_2, '153')) %>% 
+    dplyr::group_by(id_municipio) %>%
+    dplyr::summarise(n = dplyr::n()) %>% 
+    dplyr::top_n(10) %>% 
+    dplyr::rename(code_muni = id_municipio) %>% 
+    dplyr::mutate(code_muni = as.numeric(code_muni)) %>% 
+    dplyr::arrange(desc(n)), 
+  
+  geobr::read_municipality(), 
+  
+  by = 'code_muni'
+  
+)
+
+## Qual a média salarial e número de empregados da indústria de calçados nestes municípios?
+dplyr::left_join(
+
+  rais %>%
+    dplyr::filter(stringr::str_detect(cnae_2, '153')) %>%
+    dplyr::group_by(id_municipio) %>%
+    dplyr::summarise(
+      n = dplyr::n(),
+      remuneracao_media = mean(valor_remuneracao_media, na.rm = TRUE)
+    ) %>%
+    dplyr::arrange(desc(n)) %>%
+    dplyr::rename(code_muni = id_municipio) %>%
+    dplyr::mutate(code_muni = as.numeric(code_muni)),
+  
+  geobr::read_municipality(),
+  
+  by = 'code_muni'
+
+)
+
+## Quais as principais ocupações empregadas no setor (indústria calçadista) e qual a média salarial delas?
+dplyr::left_join(
+  
+  rais %>% 
+    dplyr::select(ano, sigla_uf, valor_remuneracao_media, 
+                  nacionalidade, tipo_vinculo, id_municipio, cnae_2, cbo_2002) %>%
+    dplyr::filter(stringr::str_detect(cnae_2, '153')) %>% 
+    dplyr::group_by(cbo_2002) %>%
+    dplyr::summarise(
+      n = dplyr::n(), 
+      remuneracao_media = mean(valor_remuneracao_media, na.rm = TRUE)
+    ) %>% 
+    dplyr::arrange(desc(n)),
+  
+  readr::read_delim(file = 'CBO2002 - Ocupacao.csv', delim = ';', 
+                    locale = locale(encoding = 'Latin1'), 
+                    show_col_types = FALSE) %>% 
+    janitor::clean_names() %>%
+    dplyr::rename(cbo_2002 = codigo, ocupacao = titulo),
+  
+  by = "cbo_2002"
+  
+)
+
+## O setor (indústria calçadista) emprega mais homens ou mulheres e qual a média salarial deles?
+
+dplyr::left_join(
+  
+  rais %>% 
+    dplyr::select(ano, sigla_uf, valor_remuneracao_media, 
+                  nacionalidade, tipo_vinculo, id_municipio, 
+                  cnae_2, cbo_2002, sexo) %>%
+    dplyr::filter(stringr::str_detect(cnae_2, '153')) %>% 
+    dplyr::group_by(sexo) %>%
+    dplyr::summarise(
+      n = dplyr::n(), 
+      remuneracao_media = mean(valor_remuneracao_media, na.rm = TRUE)
+    ) %>% 
+    dplyr::arrange(desc(n)), 
+  
+  tribble(
+    ~sexo, ~sexo_nome, 
+    "1", "Homem", 
+    "2", "Mulher"
+  ), 
+  
+  by = 'sexo'
+  
+)
+
+## Qual o nível de escolaridade dos empregados neste setor (indústria calçadista)?
+dic <- bdplyr("br_me_rais.dicionario") %>% 
+  bd_collect()
+
+dplyr::left_join(
+  
+  rais %>% 
+    dplyr::select(ano, sigla_uf, valor_remuneracao_media, 
+                  nacionalidade, tipo_vinculo, id_municipio, 
+                  cnae_2, cbo_2002, sexo, grau_instrucao_apos_2005) %>%
+    dplyr::filter(stringr::str_detect(cnae_2, '153')) %>% 
+    dplyr::group_by(grau_instrucao_apos_2005) %>%
+    dplyr::summarise(
+      n = dplyr::n()
+    ) %>% 
+    dplyr::arrange(dplyr::desc(n)) %>% 
+    dplyr::rename(chave = grau_instrucao_apos_2005),
+  
+  dic %<>% 
+    dplyr::filter(nome_coluna == "grau_instrucao_apos_2005") %>%
+    dplyr::select(grau_instrucao_apos_2005 = valor, chave),
+  
+  by = 'chave'
+  
+)

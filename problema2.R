950bebf1c281e77644e7f089058dcce5ae254030
Rafael Vetromillez <rafaelvetromille1@gmail.com>
2022-05-24 23:43:04 -0300

    kg

--- problema2.R
+++ problema2.R
@@ -18,32 +18,35 @@ ncm <- readr::read_csv2(
   ) %>%
   dplyr::select(CO_NCM, NO_NCM_POR)
 
-# A. Quais os top 10 produtos exportados pelo Ceará em 2019 e o desempenho do
+# A. Quais os top 10 produtos 'exportados' pelo Ceará em 2019 e o desempenho do
 # comércio desses produtos em 2021, houve uma queda ou crescimento das exportações
 # desses produtos?
 
-## 2019
+# 2019
 exp_2019 <- readr::read_csv2(
   file = 'https://balanca.economia.gov.br/balanca/bd/comexstat-bd/ncm/EXP_2019.csv'
   )
 
-dplyr::left_join(
+df1 <- dplyr::left_join(
   
-  exp_2019 %>% 
-    dplyr::filter(SG_UF_NCM == "CE") %>% 
-    dplyr::mutate(DATE = lubridate::make_date(year = CO_ANO, month = CO_MES)) %>% 
-    dplyr::group_by(CO_NCM) %>% 
-    dplyr::summarise(
-      across(.cols = c(KG_LIQUIDO, VL_FOB), .fns = sum)
-    ) %>% 
-    dplyr::arrange(desc(VL_FOB)) %>% 
-    dplyr::mutate(PART = scales::percent(VL_FOB / sum(VL_FOB), 0.01)) %>% 
-    dplyr::rename_with(.cols = c(KG_LIQUIDO, VL_FOB), .fn = ~paste0(.x, '_2019')), 
+  exp_2019 %>%
+    dplyr::filter(SG_UF_NCM == "CE") %>%
+    dplyr::mutate(DATE = lubridate::make_date(year = CO_ANO, month = CO_MES)) %>%
+    dplyr::group_by(CO_NCM) %>%
+    dplyr::summarise(across(
+      .cols = c(KG_LIQUIDO, VL_FOB), .fns = sum
+    )) %>%
+    dplyr::arrange(desc(VL_FOB)) %>%
+    dplyr::mutate(PART_2019_FOB = scales::percent(VL_FOB / sum(VL_FOB), 0.01), 
+                  PART_2019_KG  = scales::percent(KG_LIQUIDO / sum(KG_LIQUIDO), 0.01)) %>%
+    dplyr::rename_with(
+      .cols = c(KG_LIQUIDO, VL_FOB), .fn = ~paste0(.x, '_2019')
+    ),
   
-  ncm, 
+  ncm,
   
   by = 'CO_NCM'
-
+  
 )
 
 
